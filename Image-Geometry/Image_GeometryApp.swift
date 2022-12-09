e2aca5766b97278aa80e566c93446e08543efa3c
DavidEtoedia <etoedia@gmail.com>
2022-12-09 07:05:57 -0800

    Initial Commit

---
+++ Image-Geometry/Image_GeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_GeometryApp.swift
+//  Image-Geometry
+//
+//  Created by Inyene Etoedia on 09/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Image_GeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
